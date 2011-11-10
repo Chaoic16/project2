a6063a59c3f4ca7b76820364a0bf99443e77c725
David Salazar <Eagle114th@gmail.com>
2011-11-09 18:08:52 -0600

    updated webpages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
+    render_views
+    
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have selector("title", :content => "Ruby on Rails Sample App | Home")
     end
   end
 
@@ -14,12 +21,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have selector("title", :content => "Ruby on Rails Sample App | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have selector("title", :content => "Ruby on Rails Sample App | About")
     end
   end
 end
